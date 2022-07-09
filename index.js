275343006646470ec25d2831597884790b64cd86
baonguyen25 <nguyenminhgiabao2501@gmail.com>
2022-07-09 16:35:23 +0700

    create model

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ import dotenv from 'dotenv';
 
 const app = express();
 const PORT = 5000;
+dotenv.config();
+
+//CONNECT DATABASE
+mongoose.connect((process.env.MONGODB_URL), () => {
+    console.log("Connected to MongoDB")
+});
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan("common"));
-
 
 app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT}`));
